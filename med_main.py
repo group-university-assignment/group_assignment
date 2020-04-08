51490830209575ec223caf0c473f0d3f99f0bd34
Sahil A Budhwani <55096162+sahilabudhwani@users.noreply.github.com>
2020-04-08 15:22:45 +0100

    Add files via upload
    This code snippet is for running inference. This is will be integrated into the front end to display the results.

--- med_main.py
+++ med_main.py
@@ -11,3 +11,14 @@ cv2.imshow('Input image ', iputImg)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 inpInit.InputMain()
+SegmentImg=ResSeg.PredictionTrainTest(8,iputImg)
+cv2.imshow('Segmented image', SegmentImg)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+Resultclass=VGGCla.PredictVGG(SegmentImg,path1)
+if Resultclass==0:
+    print('High Grade Tumor')
+elif Resultclass==1:
+    print('Low Grade Tumor')
+else:
+    print('Image not valid')

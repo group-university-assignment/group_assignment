d8c6de52f74a3b0c7c38d58f1c4d9aba63304bc3
Kolurize <47336894+Kolurize@users.noreply.github.com>
2020-04-16 15:29:14 +0100

    Add files via upload
    v1.5

--- textcomponent.py
+++ textcomponent.py
@@ -1,50 +1,78 @@
 import nltk
+from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 import string
 from gtts import gTTS
-from docx import Document
+import textract
+import collections
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import vlc #pip install python-vlc
 
-def get_data(doc_filename) {
-    #assuming the gui processes the file and puts it in the same directory as the code
-    filename = doc_filename
-    file = open(filename, 'rt')
-    docs = file.read()
-    file.close()
+def get_data(doc_file):
+    if 'txt' in doc_file:
+        f = open(doc_file, 'rt', encoding='utf-8', errors='ignore')
+        docs = f.read()
+        f.close()
+    else:
+        docs = textract.process(doc_file).decode('utf-8')
     return docs
-    }
 
-def tts(docs) {
+def tts(docs):
     tts = gTTS(docs, lang='en')
-    return tts
-    }
+    tts.save('Recording/example.mp3')
+    #get the mp3, return a playable file
+    speech = vlc.MediaPlayer('Recording/example.mp3')
+    return speech
+    #speech.play() to play the file
+    #speech.stop() to stop the playing
 
-def extract_data(docs) {
+def extract_data(docs):
     tokens = word_tokenize(docs)
     tokens = [w.lower() for w in tokens]
     table = str.maketrans('', '', string.punctuation)
     stripped = [w.translate(table) for w in tokens]
-    stop_words = seet(stropwords.words('english'))
+    words = [word for word in stripped if word.isalpha()]
+    stop_words = set(stopwords.words('english'))
     words = [w for w in words if not w in stop_words]
     #stem words for accurate graphing
-    porter = PorterStemmer
-    stemmed = [porter.stem(word) for word in tokens]
-    
-    }
+    porter = PorterStemmer()
+    stemmed = [porter.stem(word) for word in words]
+    #extract most used words in text as indicator for topic
+    wordcount = {}
+    for word in stemmed:
+        if word not in wordcount:
+            wordcount[word]=1
+        else:
+            wordcount[word]+=1           
+    word_counter = collections.Counter(wordcount)
+    common = word_counter.most_common(3)
+    result = "The top words in the text indicate the text is about " + ', '.join(str(x[0]) for x in common)
+    return word_counter, result
 
-def vis_data(dataextr) {
-    ##build a graph using dataextr
-    ##turn it to an img
-    #return img to console
-    }
+def vis_data(word_counter):
+    lst = word_counter.most_common(10)
+    df = pd.DataFrame(lst, columns = ['Word', 'Count'])
+    df.plot.bar(x='Word', y='Count')
+    fig = plt.gcf()
+    fig.savefig("Images/test.png")
+    img = mpimg.imread("Images/test.png")
+    return img #output using plt.show(img)
 
-#make an ai for??? uh
 
-
-def runprog(document){
-    dataout = get_data(document)
+def runprog(doc_path):
+    dataout = get_data(doc_path)
     speech = tts(dataout)
-    dataextr = extract_data(document)
-    img = vis_data(dataextr)
-    return speech, img
-    }
+    word_count, result = extract_data(dataout)
+    img = vis_data(word_count)
+    return speech, img, result
+
+#testing grounds
+#dataout = get_data('the idiot pt 1.docx')
+#p = tts(dataout)
+#word_count, result = extract_data(dataout)
+#print(result)
+#img = vis_data(word_count)
+
